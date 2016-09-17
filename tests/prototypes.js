4ad3810670174cad4353dbe14b57fa9e2f7aa1f9
cvasseng <cvasseng@gmail.com>
2016-09-17 15:20:17 +0200

    Fixed parser bug

--- tests/prototypes.js
+++ tests/prototypes.js
@@ -1,5 +1,11 @@
 /** My prototype class
  * @constructor
+ * 
+ * @example My named example
+ *
+ * //Create a new instance of MyClass
+ * var myClass = new MyClass();
+ *
  */
 function MyClass () {
     
